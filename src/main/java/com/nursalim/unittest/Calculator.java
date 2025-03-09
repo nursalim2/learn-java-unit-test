a4decc0efad27a4f0ae929c48da59c2a4b398705
Nursalim <nursalim.me@gmail.com>
2025-03-09 17:30:29 +0700

    initial commit

---
+++ src/main/java/com/nursalim/unittest/Calculator.java
@@ -0,0 +1,7 @@
+package com.nursalim.unittest;
+
+public class Calculator {
+    public Integer add(Integer first, Integer second) {
+        return first + second;
+    }
+}
---
+++ src/test/java/com/nursalim/unittest/CalculatorTest.java
@@ -0,0 +1,12 @@
+package com.nursalim.unittest;
+
+import org.junit.jupiter.api.Test;
+
+public class CalculatorTest {
+    private Calculator calculator = new Calculator();
+
+    @Test
+    public void testAddSuccess() {
+        var result = calculator.add(10, 10);
+    }
+}
